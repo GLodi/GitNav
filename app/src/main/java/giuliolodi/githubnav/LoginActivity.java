a03f30404dc740c5c86ab58210a2770de70b5c78
GLodi <lodi.giulio@gmail.com>
2016-09-06 16:52:47 +0200

    Added Login Activity

---
+++ app/src/main/java/giuliolodi/githubnav/LoginActivity.java
@@ -0,0 +1,26 @@
+package giuliolodi.githubnav;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+        final Button button = (Button) findViewById(R.id.btn_login);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+}
--- app/src/main/java/giuliolodi/githubnav/MainActivity.java
+++ app/src/main/java/giuliolodi/githubnav/MainActivity.java
@@ -1,5 +1,6 @@
 package giuliolodi.githubnav;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
