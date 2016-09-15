f4fa6f1d67c6e0a706bf4f634e0d75c090d3d3da
GLodi <lodi.giulio@gmail.com>
2016-09-15 15:38:32 +0200

    Added Fragment system.

--- app/src/main/java/giuliolodi/githubnav/MainActivity.java
+++ app/src/main/java/giuliolodi/githubnav/MainActivity.java
@@ -17,7 +17,6 @@
 package giuliolodi.githubnav;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -59,16 +58,16 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        String ss = Constants.authdKey(getApplicationContext());
-        Boolean s = Constants.authdValue(getApplicationContext());
-        s.toString();
-
         // If user is not logged in, Intent to LoginActivity
         if (!Constants.authdValue(getApplicationContext())) {
             Intent intent = new Intent(this, LoginActivity.class);
             startActivity(intent);
         }
 
+        RepoFragment repoFragment = new RepoFragment();
+        android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frame, repoFragment);
+        fragmentTransaction.commit();
     }
 
     @Override
@@ -107,6 +106,22 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
+
+        switch (item.getItemId()) {
+            case R.id.nav_camera:
+                break;
+            case R.id.nav_gallery:
+                break;
+            case R.id.nav_manage:
+                break;
+            case R.id.nav_send:
+                break;
+            case R.id.nav_share:
+                break;
+            case R.id.nav_slideshow:
+                break;
+        }
+        /*
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
@@ -122,6 +137,7 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_send) {
 
         }
+        */
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
---
+++ app/src/main/java/giuliolodi/githubnav/RepoFragment.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2016 Giulio Lodi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package giuliolodi.githubnav;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RepoFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.repo_fragment, container, false);
+
+        return v;
+    }
+
+}
