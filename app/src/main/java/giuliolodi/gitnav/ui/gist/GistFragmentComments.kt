cab3f7ade2ccac49a0ccb0aa4fefe449125437ea
GLodi <lodi.giulio@gmail.com>
2017-07-07 18:32:35 +0200

    Improved GistFragmentComments.

--- app/src/main/java/giuliolodi/gitnav/ui/gist/GistFragmentComments.kt
+++ app/src/main/java/giuliolodi/gitnav/ui/gist/GistFragmentComments.kt
@@ -17,11 +17,19 @@
 package giuliolodi.gitnav.ui.gist
 
 import android.os.Bundle
+import android.support.v7.widget.DefaultItemAnimator
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import es.dmoral.toasty.Toasty
 import giuliolodi.gitnav.R
 import giuliolodi.gitnav.ui.base.BaseFragment
+import giuliolodi.gitnav.ui.user.UserActivity
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.gist_fragment_comments.*
 import org.eclipse.egit.github.core.Comment
 import javax.inject.Inject
 
@@ -31,6 +39,8 @@ import javax.inject.Inject
 class GistFragmentComments : BaseFragment(), GistContractComments.View {
 
     @Inject lateinit var mPresenter: GistContractComments.Presenter<GistContractComments.View>
+
+    private val mGistCommentList: MutableList<Comment> = mutableListOf()
 
     companion object {
         fun newInstance(gistId: String): GistFragmentComments {
@@ -54,18 +64,43 @@ class GistFragmentComments : BaseFragment(), GistContractComments.View {
 
     override fun initLayout(view: View?, savedInstanceState: Bundle?) {
         mPresenter.onAttach(this)
+
+        val llmComments = LinearLayoutManager(context)
+        llmComments.orientation = LinearLayoutManager.VERTICAL
+        gist_fragment_comments_rv.layoutManager = llmComments
+        gist_fragment_comments_rv.itemAnimator = DefaultItemAnimator()
+        gist_fragment_comments_rv.adapter = GistCommentAdapter()
+        (gist_fragment_comments_rv.adapter as GistCommentAdapter).addGistCommentList(mGistCommentList)
+
+        (gist_fragment_comments_rv.adapter as GistCommentAdapter).getImageClicks()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe { username ->
+                    startActivity(UserActivity.getIntent(context).putExtra("username", username))
+                    activity.overridePendingTransition(0,0)
+                }
     }
 
     override fun showComments(gistCommentList: List<Comment>) {
+        mGistCommentList.clear()
+        mGistCommentList.addAll(gistCommentList.toMutableList())
+
+        if (mGistCommentList.isEmpty()) {
+            gist_fragment_comments_nocomments.visibility = View.VISIBLE
+        }
     }
 
     override fun showLoading() {
+        gist_fragment_comments_progressbar.visibility = View.VISIBLE
     }
 
     override fun hideLoading() {
+        if (gist_fragment_comments_progressbar.visibility == View.VISIBLE)
+            gist_fragment_comments_progressbar.visibility = View.GONE
     }
 
     override fun showError(error: String) {
+        Toasty.error(context, error, Toast.LENGTH_LONG).show()
     }
 
 }
