0c7cf485c78ff3c59196256d2af651e450b54b4a
GLodi <lodi.giulio@gmail.com>
2017-11-22 19:02:03 +0100

    Updated gradle version. Fixed UserFragment bug.

--- app/src/main/java/giuliolodi/gitnav/ui/adapters/RepoListAdapter.kt
+++ app/src/main/java/giuliolodi/gitnav/ui/adapters/RepoListAdapter.kt
@@ -98,6 +98,7 @@ class RepoListAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     fun addRepos(repoList: List<Repository>) {
         if (!repoList.isEmpty()) {
+            if (mRepoList.isEmpty()) notifyDataSetChanged()
             val lastItemIndex = if (mRepoList.size > 0) mRepoList.size else 0
             mRepoList.addAll(repoList)
             notifyItemRangeInserted(lastItemIndex, mRepoList.size)
